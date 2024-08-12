6fce820d2afb3b7062e1081d95ca6bea8172df33
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-08-12 15:16:34 -0400

    declared functions for commands

--- server.go
+++ server.go
@@ -21,27 +21,28 @@ func (s *server) run() {
 	for cmd := range s.comamnds {
 		switch cmd.id {
 		case CMD_USER:
-			s.user()
+			s.user(cmd.client, cmd.args)
 		case CMD_JOIN:
-			s.join()
+			s.join(cmd.client, cmd.args)
 		case CMD_SERVERS:
-			s.servers()
+			s.servers(cmd.client)
 		case CMD_LANG:
-			s.language()
+			s.language(cmd.client, cmd.args)
 		case CMD_MSG:
-			s.message()
+			s.message(cmd.client, cmd.args)
 		case CMD_QUIT:
-			s.quit()
+			s.quit(cmd.client)
 
 		}
 	}
 }
 func (s *server) newClient(conn net.Conn) {
-	log.Printf("new client has connected: 5s", conn.RemoteAddr().String())
+	log.Printf("new client has connected: %s", conn.RemoteAddr().String())
 
 	c := &client{
 		conn:     conn,
 		user:     "anon",
+		language: "English",
 		commands: s.comamnds,
 	}
 	c.readInput()
