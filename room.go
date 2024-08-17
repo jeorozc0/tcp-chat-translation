89f26f209e8115a17728d98bfab9845220355fcf
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-08-16 20:00:02 -0400

    add different methods for messaging and broadcast to users

--- room.go
+++ room.go
@@ -1,16 +1,33 @@
 package main
 
-import "net"
+import (
+	"net"
+
+	lang "jeorozco.com/go/tcp-chat-translation/language"
+)
 
 type room struct {
 	name    string
 	members map[net.Addr]*client
 }
 
-func (r *room) broadcast(sender *client, msg string) {
+func (r *room) broadcast(sender *client, brd string) {
 	for addr, m := range r.members {
 		if addr != sender.conn.RemoteAddr() {
-			m.msg(msg)
+			m.brd(brd)
+		}
+	}
+}
+
+func (r *room) message(sender *client, msg string) {
+	for addr, m := range r.members {
+		if addr != sender.conn.RemoteAddr() {
+			translatedMessgae, err := lang.TranslateMsg(msg, sender.language, m.language)
+			if err != nil {
+				m.msg("Couldn't translate message from " + sender.user + ": " + msg)
+				return
+			}
+			m.msg(sender.user + ": " + translatedMessgae)
 		}
 	}
 }
